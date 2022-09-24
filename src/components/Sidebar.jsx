76e331a4f74c64c04087b1900e67498e29e8d30e
= <=>
2022-09-24 12:34:32 +0300

    add glass polymorphic mobile navigation menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import { RiCloseLine } from "react-icons/ri";
 // local imports
 import { logo } from "../assets";
 import { links } from "../assets/constants";
+import { HiOutlineMenu } from "react-icons/hi";
 
 const Sidebar = () => {
   // create a naave link component
@@ -26,13 +27,40 @@ const Sidebar = () => {
       </div>
     );
   };
+  // part of the sideBar component
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-      <NavLinks />
-    </div>
+    <>
+      {/* desktop sidebar */}
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      {/* mobile sidebar menu icon*/}
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(false)}
+          />
+        ) : (
+          <HiOutlineMenu
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(true)}
+          />
+        )}
+      </div>
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-t1 from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? "left-0" : "-left-full"
+        } `}
+      >
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
   );
 };
 
